848a3a216fa1f477718fe1ffbbd112e8fd3f9fe1
Chris Contolini <christopher.contolini@cfpb.gov>
2013-02-14 04:06:49 -0500

    Look -- a unit test!

--- specs/hmdapolis.js
+++ specs/hmdapolis.js
@@ -1,6 +1,6 @@
 describe("HMDA game", function() {
 
-  it("should have its number of players greater than the current player", function() {
+  it("should have its total number of players be greater than the current player's number", function() {
     expect(HMDA.game.get('numPlayers')).toBeGreaterThan(HMDA.game.get('currentPlayer'));
   });
 
